8c1ff8910afcf478ce81c665bec0076154f8c63e
ofwebdev <nkomorfaruk@gmail.com>
2023-01-25 21:44:41 +0600

    chakra-ui issue fixed

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {Heading, Box} from '@chakra-ui/react'
+import {Heading, Box, Center} from '@chakra-ui/react'
 
 function header() {
   return (
     <Box w="90%" m="0 auto">
-        <Heading align="center" py="5" fontSize='4xl'>Pixels</Heading>
+      <Center>
+        <Heading py="5" fontSize='4xl'>Pixels</Heading>
+      </Center>
     </Box>
   )
 }
