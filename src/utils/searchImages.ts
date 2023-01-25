f2271066e72c49b6136b35d91c8d395db14adb9a
ofwebdev <nkomorfaruk@gmail.com>
2023-01-25 22:08:08 +0600

    finally deploy

--- src/utils/searchImages.ts
+++ src/utils/searchImages.ts
@@ -1,27 +1,33 @@
-import axios from "axios";
 
+import axios from "axios";
 import { getQuery } from "./cookies";
 
-const searchImages = async ({ setPage, setHasMore, page = 1 }) => {
-    const query = getQuery();
-
-    if (!query) return []
-
-    const {data} = await axios.get('/api/getPhotos/', {
-        params: { query, page: 1 }
-    })
-
-    setPage((prev) => prev + 1)
-
-    
-	if (data.length < 15) {
-		setHasMore(false)
-	} else {
-		setHasMore(true)
-	}
-
-    return data
+interface SearchImagesProps {
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  setHasMore: React.Dispatch<React.SetStateAction<boolean>>;
+  page?: number;
 }
 
+const searchImages = async (props: SearchImagesProps) => {
+  const { setPage, setHasMore, page = 1 } = props;
+  const query = getQuery();
+
+  if (!query) return [];
+
+  const { data } = await axios.get("/api/getPhotos/", {
+    params: { query, page: 1 },
+  });
+
+  setPage((prev) => prev + 1);
+
+  if (data.length < 15) {
+    setHasMore(false);
+  } else {
+    setHasMore(true);
+  }
+
+  return data;
+};
 
 export default searchImages;
+
